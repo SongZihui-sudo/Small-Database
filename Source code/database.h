49ad1772832c408c1cfc49b782f77d7809be6075
宋子慧 <77034643+SongZihui-sudo@users.noreply.github.com>
2021-10-11 21:05:40 +0800

    Update database.h

--- Source code/database.h
+++ Source code/database.h
@@ -8,7 +8,6 @@
     //创建用户
     //用户数据以txt文档形式储存
     int size;
-
     typedef class user{
     private:
         std::string username; 
@@ -38,14 +37,21 @@
             database *NeXt; //链表指针
             std::string name; //数据名
             int number; //数据号
+            const std::string data_exit = "data-exit"; 
+            const std::string data_delete = "data-delete";
+            const std::string data_add = "data-add";
+            const std::string data_change = "data-change"; 
+            const std::string write = "data-write";
+            const std::string find_d = "data-find -d";
         public:            
             database() = default;
             int Change(class database *change); //修改数据
             int Delete(class database *Del); //删除数据
-            int Search(database *init); //查找
+            int Search(class database *find_end); //查找
             int add(class database *add); //插入
             int filewrite();
             int fileread(database *init);
+            int print_to_file(database *printtofile);
     }database;
 //命令行
     typedef class Command{
@@ -56,9 +62,8 @@
             const std::string CrEat = "creat";
             const std::string Find = "find";    
             const std::string sudo = "sudo" ;
-            const std::string read = "read";
-            const std::string write = "write";
-            const std::string find_d = "find -d";
+            const std::string login = "login";
+            const std::string exit = "exit";
         public:
             Command() = default;
             void help();        //帮助
@@ -71,9 +76,14 @@
         std::cout<<"2.press /change/ to change meeage"<<"\n";
         std::cout<<"3.press /delete/ to delete a dataline"<<"\n";
         std::cout<<"4.press /creat/ to creat a dataline"<<"\n";
-        std::cout<<"5.press /read/ to read the dataline"<<"\n";
-        std::cout<<"6.press /write/ to write the dataline"<<"\n";
-        std::cout<<"7.press /find -d/ to find the data"<<"\n";
+        std::cout<<"5.press /login/ to login the dataline"<<"\n";
+        std::cout<<"6.press /data-write/ to write the dataline"<<"\n";
+        std::cout<<"7.press /data-find -d/ to find the data"<<"\n";
+        std::cout<<"8.press /data-exit/ to exit the dataline"<<"\n";
+        std::cout<<"9.press /data-delete/ to delete the data in dataline"<<"\n";
+        std::cout<<"10.press /data-add/ to add the data in dataline"<<"\n";
+        std::cout<<"11.press /data-change/ to change the data in dataline"<<"\n";
+        std::cout<<"12.press /exit/ to exit the program"<<"\n";
     }
     //命令行
     int Command::comander(class Command *c,class user *u,class database *data){
@@ -100,16 +110,12 @@
                 std::cin>>filename;
                 u->search(filename);
             }
-            else if (command == c->read){
+            else if (command == c->login){
                 data->fileread(data);
             }
-            else if (command == c->write){
-                data->filewrite();
+            else if (command == c->exit){
+                return OK;
             }
-            else if ( command == c->find_d ){
-                data->Search(data);
-            }
-            
             else{
                 std::cout<<"Command is not found!"<<"\n";
             }
